f72f969440b2c0d1ba7351c1d35d121c4ed199c9
Maciej Tabaczynski <maciej.tabaczynski@gmail.com>
2019-11-04 19:01:46 +0100

    landing page cd

--- my-app/src/components/HomeContact.js
+++ my-app/src/components/HomeContact.js
@@ -1,8 +1,43 @@
 import React from 'react';
+import Pattern from '../assets/Decoration.svg';
+import Facebook from '../assets/Facebook.svg';
+import Instagram from '../assets/Instagram.svg';
 
 const HomeContact = () => {
   return (
     <>
+      <div className={"HomeContact"}>
+        <div className={"HomeContactUs"}>
+        <h2>Skontaktuj się z nami</h2>
+        <img src={Pattern} alt=""/>
+        </div>
+        <form>
+          <label>
+             Wpisz swoje imię <br></br>
+            <input type="text" name="name" placeholder= {"Krzysztof"} />
+              </label>
+              <label>
+             Wpisz swój email <br></br>
+            <input type="email" name="name" placeholder= {"abc@xyz.pl"} />
+              </label>
+              <label>
+             Wpisz swoją wiadomość <br></br>
+            <textarea name="name" placeholder= {"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip exaea commodo consequat."} />
+              </label>
+              <button>Wyślij</button>
+         </form>
+      </div>
+      <div className={"Footer"}>
+        <span>Copyright by Coders Lab</span>
+        <div className={"Media"}>
+          <a href={"https://facebook.com"}>
+            <img src={Facebook} alt="facebook"/>
+          </a>
+          <a href={"https://instagram.com"}>
+            <img src={Instagram} alt="instagram"/>
+          </a>
+        </div>
+      </div>
     </>
   );
 };
--- my-app/src/components/HomeHelp.js
+++ my-app/src/components/HomeHelp.js
@@ -1,8 +1,39 @@
 import React from 'react';
+import Pattern from '../assets/Decoration.svg';
 
 const HomeHelp = () => {
   return (
     <>
+      <div className={"HomeHelp"}>
+        <h2> Komu pomagamy?</h2>
+        <img src={Pattern} alt="pattern"/>
+        <div className={"HelpPlaces"}>
+          <button className={"HelpButton"}>Fundacjom</button>
+          <button className={"HelpButton"}>Organizacjom pozarządowym</button>
+          <button className={"HelpButton"}>Lokalnym zbiórkom</button>
+        </div>
+        <div>
+          <p className={"HelpIntro"}>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+        </div>
+      </div>
+      <div className={"HelpOrganizations"}>
+        <div>
+          <h3>Organizacja "Lorem Ipsum 1" <span>Egestas, sed, tempus</span></h3>
+        <span className={"HelpOrganizationsCursive"}>Quis varius quam quiusque id diam vel quam elementum pulvinar. </span>
+        <hr/>
+        </div>
+        <div><h3>Organizacja "Lorem Ipsum 2" <span>Ut, aliquam, purus, sit, amet</span></h3>
+        <span className={"HelpOrganizationsCursive"}>Hendrerit gravida rutrum quisque non tellus orci ac auctor augue. </span>
+        <hr/>
+        </div>
+        <div><h3>Organizacja "Lorem Ipsum 3" <span>Mi, quis, hendrerit, dolor</span></h3>
+        <span className={"HelpOrganizationsCursive"}>Scelerisque in dictum non consectetur a erat nam. </span>
+        </div>
+      </div>
+      <div className={"HelpPages"}>
+        <button className={"HelpPage"}>1</button>
+        <button className={"HelpPage"}>2</button>
+      </div>
     </>
   );
 };
